53d9b878a7e61b569339be0b9511ecda94e10866
Kevin <trupiakevin@gmail.com>
2026-02-05 21:19:21 +0100

    feat : task completata

--- index.js
+++ index.js
@@ -2,9 +2,51 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(express.static('public'))
+
+const posts = [
+    {
+        titolo: "ciambellone",
+        contenuto: "dolce fatto in casa",
+        immagine: "/images/ciambellone.jpg",
+        tags: ["dolce"]
+    },
+    {
+        titolo: "cracker barbabietola",
+        contenuto: "dolce fatto in casa",
+        immagine: "/images/cracker_barbabietola.jpg",
+        tags: ["snack"]
+    },
+    {
+        titolo: "pane fritto dolce",
+        contenuto: "tradizionale",
+        immagine: "/images/pane_fritto_dolce.jpg",
+        tags: ["dolce"]
+    },
+    {
+        titolo: "pasta barbabietola",
+        contenuto: "pasta sana",
+        immagine: "/images/pasta_barbabietola.jpg",
+        tags: ["salato"]
+    },
+    {
+        titolo: "torta paesana",
+        contenuto: "dolce fatto in casa",
+        immagine: "/images/torta_paesana.jpg",
+        tags: ["dolce"]
+    }
+];
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Server del mio blog')
 })
+
+app.get('/bacheca', (req, res) => {
+    res.json(posts)
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server avviato su http://localhost:${port}`)
 })
+
